0e9d168fad27f03dea8a4ee8b04ebc0b59a721c7
Jenil Khandhara <jkhandhara@microsoft.com>
2023-01-20 16:03:39 +0530

    Adding store so it is visible in extension

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,8 @@
 import bookmarkNewsReducer from "./reducers/bookmarkNewsReducer";
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 
-const store = createStore(bookmarkNewsReducer)
+const store = createStore(bookmarkNewsReducer, composeWithDevTools())
 
 export default store
